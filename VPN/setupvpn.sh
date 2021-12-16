8437e3f04b8c963294d1ba9a354fd4552845684b
stavdnb <m.kolesnichenko@devex.pro>
2021-12-16 21:43:04 +0300

    transfer from repository 05-2021

---
+++ VPN/setupvpn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.4.list
+echo "deb http://repo.pritunl.com/stable/apt focal main" > /etc/apt/sources.list.d/pritunl.list
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+apt-get --assume-yes update
+apt-get --assume-yes upgrade
+apt-get --assume-yes install pritunl mongodb-org
+systemctl start pritunl mongod
+systemctl enable pritunl mongod
