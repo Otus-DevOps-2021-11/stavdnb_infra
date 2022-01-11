8379f86e1a712b227a2c58a6289e0d103b9b710a
stavdnb <m.kolesnichenko@devex.pro>
2022-01-11 22:27:44 +0300

    added additional task

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,15 +7,15 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "AQAAAAAMZMhBAATuwamLlOgVyUoNoaKTbtzEo54"
-  cloud_id  = "b1g1pmd1srv113b35h52"
-  folder_id = "b1gcaact1bh30f0jmbkt"
-  zone      = "ru-central1-a"
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
-  name = "reddit-app"
-
+  name  = "reddit-app-${count.index}"
+  count = var.count_of_instances
   resources {
     cores  = 2
     memory = 2
@@ -24,27 +24,27 @@ resource "yandex_compute_instance" "app" {
   boot_disk {
     initialize_params {
       # Указать id образа созданного в предыдущем домашем задании
-      image_id = "fd8e66hr2jdlf8olspkh"
+      image_id = var.image_id
     }
   }
 
   network_interface {
     # Указан id подсети default-ru-central1-a
-    subnet_id = "e9bi6t4kakg8oshloqtm"
+    subnet_id = var.subnet_id
     nat       = true
   }
 
   metadata = {
-ssh-keys = "ubuntu:${file("~/.ssh/appuser.pub")}"
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
 
   connection {
     type  = "ssh"
-    host = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    host  = self.network_interface.0.nat_ip_address
     user  = "ubuntu"
     agent = false
     # путь до приватного ключа
-    private_key = file("~/.ssh/appuser")
+    private_key = "${file(var.private_key_path)}"
   }
 
   provisioner "file" {
