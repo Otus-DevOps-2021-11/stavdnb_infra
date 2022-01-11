3bc3dc46d5e71a480298339113c543416441e132
stavdnb <m.kolesnichenko@devex.pro>
2022-01-11 22:34:23 +0300

    fix-1_added additional task

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,5 @@
-terraform {
-  required_providers {
-    yandex = {
-      version = "~> 0.35"
-    }
-  }
-}
-
 provider "yandex" {
+  version                  = "0.35"
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
