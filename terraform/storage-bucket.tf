00d054826300dc6d70648bc7b7f3be1ae2c012ed
stavdnb <m.kolesnichenko@devex.pro>
2022-01-17 20:05:10 +0300

    fix_6

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,14 +1,14 @@
 provider "yandex" {
   version                  = "0.35"
-  service_account_key_file = var.service-account-key-file
-  cloud_id                 = var.cloud-id
-  folder_id                = var.folder-id
-  zone                     = var.zone-id
+  service_account_key_file = "var.service-account-key-file"
+  cloud_id                 = "var.cloud-id"
+  folder_id                = "var.folder-id"
+  zone                     = "var.zone-id"
 }
 
 resource "yandex_storage_bucket" "otus-storage-bucket" {
-  bucket        = var.bucket-name
-  access_key    = var.access-key
-  secret_key    = var.secret-key
+  bucket        = "bucket-name"
+  access_key    = "access-key"
+  secret_key    = "secret-key"
   force_destroy = "true"
 }
