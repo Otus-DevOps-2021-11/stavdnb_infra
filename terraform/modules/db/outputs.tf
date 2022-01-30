e702945fe4210614ae988de00ee592609bed81d5
stavdnb <m.kolesnichenko@devex.pro>
2022-01-30 20:33:26 +0300

    ansible-2

--- terraform/modules/db/outputs.tf
+++ terraform/modules/db/outputs.tf
@@ -4,3 +4,6 @@ output "external_ip_address_db" {
 output "internal_ip_address_db" {
   value = yandex_compute_instance.db.network_interface.0.ip_address
 }
+output "local_ip_address_db" {
+  value = yandex_compute_instance.db.network_interface.0.ip_address
+}
